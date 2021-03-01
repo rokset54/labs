976e423aa13a928a7659640ef958cd00badae51a
user1 <user1@A518-05>
2021-03-01 13:02:59 +0300

    PythonCode

--- lab1.py
+++ lab1.py
@@ -1,2 +1,20 @@
 
 print("Hello Python from Visual Studio")
+s = "*"*30
+print(s)
+print("New project")
+print(s)
+
+#Простейшая арифметика задание 5 (дома)
+import math
+
+days = int(input('Введите количество дней: '))
+
+percent = float(input('Введите процент скидки (например 10;15.5): '))
+percent = 1 - (percent / 100)
+
+first_sum = float(input('Сумма в долларах: '))
+print()
+final_sum = (first_sum + 3 * days) * percent
+print( 'Итоговая сумма: {0}$'.format(final_sum) )
+input()
